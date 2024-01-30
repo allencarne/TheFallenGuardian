2b6a4f8ea71fa0ff20b380c1c7a225c2c3dfa95d
allencarne <34139066+allencarne@users.noreply.github.com>
2024-01-29 18:16:20 -0800

    Aimer Based on Control Scheme

--- The Fallen Guardian/Assets/_Scripts/Aimer.cs
+++ The Fallen Guardian/Assets/_Scripts/Aimer.cs
@@ -1,50 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Aimer : MonoBehaviour
 {
     PlayerInputHandler inputHandler;
     Camera mainCamera;
     public bool isControllerInput;
+    PlayerInput playerInput;
 
     private void Awake()
     {
         inputHandler = GetComponentInParent<PlayerInputHandler>();
+        playerInput = GetComponentInParent<PlayerInput>();
         mainCamera = Camera.main;
     }
 
     private void Update()
     {
-        if (inputHandler != null)
+        if (inputHandler != null && playerInput)
         {
-            Rotate();
+            if (playerInput.currentControlScheme == "Gamepad")
+            {
+                RotateOnGamePad();
+            }
+
+            if (playerInput.currentControlScheme == "Keyboard")
+            {
+                RotateOnKeyboard();
+            }
         }
     }
 
-    public void Rotate()
+    void RotateOnKeyboard()
     {
-        if (isControllerInput)
-        {
-            // Get the look input values
-            float horizontalLook = inputHandler.LookInput.x;
-            float verticalLook = inputHandler.LookInput.y;
+        Vector3 mouseWorldPosition = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, mainCamera.nearClipPlane));
 
-            // Calculate the rotation angle based on input
-            float angle = Mathf.Atan2(verticalLook, horizontalLook) * Mathf.Rad2Deg;
+        // Calculate the rotation angle based on mouse position
+        float angle = Mathf.Atan2(mouseWorldPosition.y - transform.position.y, mouseWorldPosition.x - transform.position.x) * Mathf.Rad2Deg;
 
-            // Apply rotation to the Aimer
-            transform.rotation = Quaternion.Euler(0f, 0f, angle);
-        }
-        else
-        {
-            Vector3 mouseWorldPosition = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, mainCamera.nearClipPlane));
+        // Apply rotation to the Aimer
+        transform.rotation = Quaternion.Euler(0f, 0f, angle);
+    }
 
-            // Calculate the rotation angle based on mouse position
-            float angle = Mathf.Atan2(mouseWorldPosition.y - transform.position.y, mouseWorldPosition.x - transform.position.x) * Mathf.Rad2Deg;
+    void RotateOnGamePad()
+    {
+        // Get the look input values
+        float horizontalLook = inputHandler.LookInput.x;
+        float verticalLook = inputHandler.LookInput.y;
 
-            // Apply rotation to the Aimer
-            transform.rotation = Quaternion.Euler(0f, 0f, angle);
-        }
+        // Calculate the rotation angle based on input
+        float angle = Mathf.Atan2(verticalLook, horizontalLook) * Mathf.Rad2Deg;
+
+        // Apply rotation to the Aimer
+        transform.rotation = Quaternion.Euler(0f, 0f, angle);
     }
 }
