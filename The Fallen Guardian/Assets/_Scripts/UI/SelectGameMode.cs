9f5210701b0dcc9fcc4ed773a34a6aaa44fbfafd
allencarne <34139066+allencarne@users.noreply.github.com>
2024-02-01 07:20:41 -0800

    UI for selecting game mode

--- The Fallen Guardian/Assets/_Scripts/UI/SelectGameMode.cs
+++ The Fallen Guardian/Assets/_Scripts/UI/SelectGameMode.cs
@@ -6,29 +6,53 @@ public class SelectGameMode : MonoBehaviour
 {
     [SerializeField] GameObject gameModePanel;
 
+    [SerializeField] GameObject singlePlayerPanel;
+    [SerializeField] GameObject localMultiplayerPanel;
+
     private void Start()
     {
         gameModePanel.SetActive(true);
+
+        singlePlayerPanel.SetActive(false);
+        localMultiplayerPanel.SetActive(false);
     }
 
     public void SinglePlayer()
     {
         PlayerPrefs.SetInt("SelectedGameMode", 0);
 
-        gameModePanel.SetActive(false);
+        ModeSelected();
     }
 
     public void LocalMultiPlayer()
     {
         PlayerPrefs.SetInt("SelectedGameMode", 1);
 
-        gameModePanel.SetActive(false);
+        ModeSelected();
     }
 
     public void OnlineMultiPlayer()
     {
         PlayerPrefs.SetInt("SelectedGameMode", 2);
 
+        ModeSelected();
+    }
+
+    void ModeSelected()
+    {
         gameModePanel.SetActive(false);
+
+        switch (PlayerPrefs.GetInt("SelectedGameMode"))
+        {
+            case 0:
+                singlePlayerPanel.SetActive(true);
+                break;
+            case 1:
+                localMultiplayerPanel.SetActive(true);
+                break;
+            case 2:
+                // Online Mulitplayer
+                break;
+        }
     }
 }
