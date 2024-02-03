3d0ee6c371d085c67131ac5d94498aa52cf0ef94
allencarne <34139066+allencarne@users.noreply.github.com>
2024-02-03 13:43:36 -0800

    IDamageable Interface Added

---
+++ The Fallen Guardian/Assets/_Scripts/Combat/DamageOnTrigger.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageOnTrigger : MonoBehaviour
+{
+    [SerializeField] int amountofDamage;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        IDamageable damageable = collision.GetComponent<IDamageable>();
+
+        if (damageable != null)
+        {
+            damageable.TakeDamage(amountofDamage);
+
+            Destroy(gameObject);
+        }
+    }
+}
