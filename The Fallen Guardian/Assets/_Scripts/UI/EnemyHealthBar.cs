fcc22ad9e71bf136a9796d12780a47020ee770e2
allencarne <34139066+allencarne@users.noreply.github.com>
2024-02-20 20:27:00 -0800

    Enemy Healthbar

---
+++ The Fallen Guardian/Assets/_Scripts/UI/EnemyHealthBar.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealthBar : MonoBehaviour
+{
+    Enemy enemy;
+
+    [SerializeField] Image healthBarFront;
+    [SerializeField] Image healthBarBack;
+    [SerializeField] Canvas EnemyUI;
+    [HideInInspector] public float chipSpeed = 2f;
+    [HideInInspector] public float lerpTimer;
+
+    public void Awake()
+    {
+        enemy = GetComponent<Enemy>();
+    }
+
+    public void Start()
+    {
+        enemy.health = enemy.maxHealth;
+    }
+
+    public void Update()
+    {
+        enemy.health = Mathf.Clamp(enemy.health, 0, enemy.maxHealth);
+
+        UpdateHealthUI();
+    }
+
+    void UpdateHealthUI()
+    {
+        float fillFront = healthBarFront.fillAmount;
+        float fillBack = healthBarBack.fillAmount;
+        float healthFraction = enemy.health / enemy.maxHealth;
+
+        if (fillBack > healthFraction)
+        {
+            healthBarFront.fillAmount = healthFraction;
+            healthBarBack.color = Color.white;
+            lerpTimer += Time.deltaTime;
+            float percentComplete = lerpTimer / chipSpeed;
+            percentComplete = percentComplete * percentComplete;
+            healthBarBack.fillAmount = Mathf.Lerp(fillBack, healthFraction, percentComplete);
+        }
+
+        if (fillFront < healthFraction)
+        {
+            healthBarBack.color = Color.green;
+            healthBarBack.fillAmount = healthFraction;
+            lerpTimer += Time.deltaTime;
+            float percentComplete = lerpTimer / chipSpeed;
+            percentComplete = percentComplete * percentComplete;
+            healthBarFront.fillAmount = Mathf.Lerp(fillFront, healthBarBack.fillAmount, percentComplete);
+        }
+    }
+}
