65264ae79218f2270016c3e0aa9db417fb7f7ad4
allencarne <34139066+allencarne@users.noreply.github.com>
2024-03-01 21:46:05 -0800

    Added Coin Item

---
+++ The Fallen Guardian/Assets/_Scripts/UI/Inventory/Item.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "ScriptableObjects/Inventory/Item")]
+public class Item : ScriptableObject
+{
+    new public string name;
+    public Sprite icon;
+
+    public virtual void Use()
+    {
+        //Debug.Log("using " + name);
+    }
+
+    public void RemoveFromInventory()
+    {
+        //Inventory.instance.Remove(this);
+    }
+}
