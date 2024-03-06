52700b5f75f0e9e0104284109a09eeba553eb287
allencarne <34139066+allencarne@users.noreply.github.com>
2024-03-05 20:56:47 -0800

    Visual for weapon equipment

--- The Fallen Guardian/Assets/_Scripts/Characters/Player/PlayerEquipment.cs
+++ The Fallen Guardian/Assets/_Scripts/Characters/Player/PlayerEquipment.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-using static EquipmentManager;
 
 public class PlayerEquipment : MonoBehaviour
 {
@@ -26,6 +26,45 @@ public class PlayerEquipment : MonoBehaviour
 
     void OnEquipmentChanged(Equipment newItem, Equipment oldItem)
     {
+        if (newItem == null)
+            return;
 
+        Weapon equippedWeapon = newItem as Weapon;
+
+        if (equippedWeapon != null)
+        {
+            switch (equippedWeapon.weaponType)
+            {
+                case WeaponType.Sword:
+                    Debug.Log("Sword is equipt");
+                    // Sword is Equipped
+                    Sword.enabled = true;
+                    SwordAnimator.enabled = true;
+                    Sword.sprite = equippedWeapon.weaponSprite;
+                    // Disable other weapon sprites and animators
+                    break;
+                case WeaponType.Staff:
+                    Debug.Log("Staff is equipt");
+                    // Staff is Equipped
+                    //Staff.enabled = true;
+                    //StaffAnimator.enabled = true;
+                    // Disable other weapon sprites and animators
+                    break;
+                case WeaponType.Bow:
+                    Debug.Log("Bow is equipt");
+                    // Bow is Equipped
+                    //Bow.enabled = true;
+                    //BowAnimator.enabled = true;
+                    // Disable other weapon sprites and animators
+                    break;
+                case WeaponType.Dagger:
+                    Debug.Log("Dagger is equipt");
+                    // Dagger is Equipped
+                    //Dagger.enabled = true;
+                    //DaggerAnimator.enabled = true;
+                    // Disable other weapon sprites and animators
+                    break;
+            }
+        }
     }
 }
--- The Fallen Guardian/Assets/_Scripts/Items/Weapon.cs
+++ The Fallen Guardian/Assets/_Scripts/Items/Weapon.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Weapon", menuName = "ScriptableObjects/Inventory/Equipment/Weapon")]
 public class Weapon : Equipment
 {
+    public Sprite weaponSprite;
     public WeaponType weaponType;
 }
 
