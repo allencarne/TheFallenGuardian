f53907dd883e863c2187b461a7d54be11d0b7f90
allencarne <34139066+allencarne@users.noreply.github.com>
2024-03-08 17:54:27 -0800

    Ability Tree Now Working

--- The Fallen Guardian/Assets/_Scripts/UI/BeginnerAbilityTree.cs
+++ The Fallen Guardian/Assets/_Scripts/UI/BeginnerAbilityTree.cs
@@ -1,26 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BeginnerAbilityTree : MonoBehaviour
 {
     [SerializeField] GameObjectRuntimeSet playerReference;
     PlayerAbilities playerAbilities;
 
-    [SerializeField] ScriptableObject frailSlash;
+    [Header("Level 1")]
+    [SerializeField] ScriptableObject level1Ability;
+    [SerializeField] Image level1AbilityImage;
 
-    [SerializeField] ScriptableObject lungeStrike;
-    [SerializeField] ScriptableObject softSwing;
+    [Header("Level 5")]
+    [SerializeField] ScriptableObject level5Ability;
+    [SerializeField] ScriptableObject level5Ability2;
 
-    public void OnPlayerJoin()
+    private void Start()
     {
-        StartCoroutine(Delay());
+        // Set the level1Ability Icon to level1AbilityImage
+        SetAbilityIcon(level1Ability, level1AbilityImage);
+
+        InitializePlayerReference();
     }
 
-    IEnumerator Delay()
+    private void SetAbilityIcon(ScriptableObject ability, Image abilityImage)
     {
-        yield return new WaitForSeconds(1);
+        if (ability != null && abilityImage != null)
+        {
+            // Assuming the ability has a field named 'icon' of type Sprite
+            Sprite icon = (Sprite)ability.GetType().GetField("icon").GetValue(ability);
+            if (icon != null)
+            {
+                abilityImage.sprite = icon;
+            }
+            else
+            {
+                Debug.LogWarning("Icon not found for the ability: " + ability.name);
+            }
+        }
+    }
 
+    public void InitializePlayerReference()
+    {
         if (playerReference.items.Count > 0)
         {
             playerAbilities = playerReference.GetItemIndex(0).GetComponent<PlayerAbilities>();
@@ -29,6 +51,9 @@ public class BeginnerAbilityTree : MonoBehaviour
 
     public void OnFrailSlashSelected()
     {
-
+        if (playerAbilities.basicAttackBehaviourReference == null)
+        {
+            playerAbilities.basicAttackBehaviourReference = level1Ability;
+        }
     }
 }
