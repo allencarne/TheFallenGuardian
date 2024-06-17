86d0cd0a34281e4c31adad6c097e8935baf18d24
allencarne <34139066+allencarne@users.noreply.github.com>
2024-06-17 11:11:53 -0700

    Hermit Mobility Ability Started

--- The Fallen Guardian/Assets/_Scripts/Characters/Enemy/Hermit.cs
+++ The Fallen Guardian/Assets/_Scripts/Characters/Enemy/Hermit.cs
@@ -115,7 +115,7 @@ public class Hermit : Enemy
             // Set Cast Bar Color
             castBar.color = Color.green;
 
-            GameObject basic = Instantiate(basicPrefab, vectorToTarget, Quaternion.identity, transform);
+            GameObject basic = Instantiate(basicPrefab, vectorToTarget, Quaternion.identity);
 
             // Cannot Hit Self with Attack
             Physics2D.IgnoreCollision(basic.GetComponent<Collider2D>(), gameObject.GetComponent<Collider2D>());
@@ -178,9 +178,12 @@ public class Hermit : Enemy
     #region Mobility
 
     [Header("Mobility")]
-    [SerializeField] GameObject mobilityPrefab;
-    [SerializeField] GameObject mobilityHitEffect;
     [SerializeField] GameObject mobilitTelegraph;
+    GameObject mobilityTelegraqphInstance;
+
+    [SerializeField] GameObject mobilityStartPrefab;
+    [SerializeField] GameObject mobilityEndPrefab;
+    [SerializeField] GameObject mobilityHitEffect;
 
     [Header("Stats")]
     [SerializeField] int mobilityDamage;
@@ -188,10 +191,162 @@ public class Hermit : Enemy
 
     [Header("Time")]
     [SerializeField] float mobilityCastTime;
+    [SerializeField] float mobilityImpactTime;
     [SerializeField] float mobilityRecoveryTime;
     [SerializeField] float mobilityCoolDown;
 
+    protected override void MobilityState()
+    {
+        modifiedCastTime = mobilityCastTime / CurrentAttackSpeed;
 
+        if (castBar.color == Color.yellow)
+        {
+            // Increase cast bar time once per second
+            castBarTime += Time.deltaTime;
+
+            // If we are not interrupted, Update the cast bar
+            UpdateCastBar(castBarTime, modifiedCastTime);
+        }
+
+        if (crowdControl.IsInterrupted)
+        {
+            wasInterrupted = true;
+
+            if (mobilityTelegraqphInstance)
+            {
+                Destroy(mobilityTelegraqphInstance);
+            }
+
+            if (castBar.color != Color.green)
+            {
+                // ResetCast Time
+                castBarTime = 0;
+
+                // Set Cast Bar Color
+                castBar.color = Color.red;
+
+                // State Transition
+                enemyState = EnemyState.Idle;
+
+                StartCoroutine(ResetCastBar());
+                return;
+            }
+        }
+
+        if (canMobility && target != null && !hasAttacked)
+        {
+            hasAttacked = true;
+            canMobility = false;
+
+            // Set Cast Bar Color
+            castBar.color = Color.yellow;
+
+            // Calculate the direction from the enemy to the target
+            directionToTarget = (target.position - transform.position).normalized;
+
+            // Calculate the Range required to hit the target
+
+            // Calculate the position for the basicPrefab
+            vectorToTarget = (Vector2)transform.position + directionToTarget * mobilityRange;
+
+            // Play attack animation
+            enemyAnimator.Play("Mobility Cast");
+            enemyAnimator.SetFloat("Horizontal", directionToTarget.x);
+            enemyAnimator.SetFloat("Vertical", directionToTarget.y);
+
+            // Instantiate the telegraph
+            mobilityTelegraqphInstance = Instantiate(mobilitTelegraph, vectorToTarget, Quaternion.identity, transform);
+
+            FillTelegraph fillTelegraph = mobilityTelegraqphInstance.GetComponent<FillTelegraph>();
+            if (fillTelegraph != null)
+            {
+                fillTelegraph.FillSpeed = mobilityCastTime + mobilityImpactTime + mobilityRecoveryTime;
+            }
+
+            // Timers
+            StartCoroutine(MobilityImpact());
+            StartCoroutine(MobilityCoolDown());
+        }
+
+        if (canImpact)
+        {
+            canImpact = false;
+
+            StartCoroutine(MobilityRecoveryTime());
+        }
+    }
+
+    IEnumerator MobilityImpact()
+    {
+        UpdateCastBar(castBarTime, modifiedCastTime);
+
+        yield return new WaitForSeconds(modifiedCastTime);
+
+        if (!wasInterrupted)
+        {
+            // Animate
+            enemyAnimator.Play("Mobility Impact");
+
+            // Set Cast Bar Color
+            castBar.color = Color.green;
+
+            // Delay
+            StartCoroutine(MobilityImpactDelay());
+        }
+        else
+        {
+            wasInterrupted = false;
+
+            hasAttacked = false;
+        }
+    }
+
+    IEnumerator MobilityImpactDelay()
+    {
+        yield return new WaitForSeconds(mobilityImpactTime);
+
+        canImpact = true;
+
+        castBarTime = 0;
+        StartCoroutine(EndCastBar());
+    }
+
+    IEnumerator MobilityRecoveryTime()
+    {
+        // Animate
+        enemyAnimator.Play("Mobility Recovery");
+
+        float modifiedRecoveryTime = mobilityRecoveryTime / CurrentAttackSpeed;
+
+        yield return new WaitForSeconds(modifiedRecoveryTime);
+
+        GameObject mobility = Instantiate(mobilityStartPrefab, vectorToTarget, Quaternion.identity);
+
+        // Cannot Hit Self with Attack
+        Physics2D.IgnoreCollision(mobility.GetComponent<Collider2D>(), gameObject.GetComponent<Collider2D>());
+
+        DamageOnTrigger damageOnTrigger = mobility.GetComponent<DamageOnTrigger>();
+        if (damageOnTrigger != null)
+        {
+            damageOnTrigger.AbilityDamage = mobilityDamage;
+            damageOnTrigger.CharacterDamage = CurrentDamage;
+            damageOnTrigger.HitEffect = mobilityHitEffect;
+        }
+
+        hasAttacked = false;
+
+        enemyState = EnemyState.Idle;
+    }
+
+    IEnumerator MobilityCoolDown()
+    {
+        // Adjust cooldown time based on cooldown reduction
+        float modifiedCooldown = mobilityCoolDown / CurrentCDR;
+
+        yield return new WaitForSeconds(modifiedCooldown);
+
+        canMobility = true;
+    }
 
     #endregion
 
