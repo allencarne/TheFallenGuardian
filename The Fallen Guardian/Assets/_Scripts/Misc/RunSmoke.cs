7ad187614fee5fc74a731f1157b859f9b96eec80
allencarne <34139066+allencarne@users.noreply.github.com>
2024-06-22 22:16:43 -0700

    Added Footsteps

--- The Fallen Guardian/Assets/_Scripts/Misc/RunSmoke.cs
+++ The Fallen Guardian/Assets/_Scripts/Misc/RunSmoke.cs
@@ -5,9 +5,45 @@ using UnityEngine;
 public class RunSmoke : MonoBehaviour
 {
     [SerializeField] GameObject runSmoke;
+    [SerializeField] GameObject footStep;
+
+    Vector3 offsetRight = new Vector3(0.1f, 0, 0);
+    Vector3 offsetLeft = new Vector3(-0.1f, 0, 0);
+
+    Quaternion rotation = Quaternion.Euler(0, 0, 90);
+
+    Vector3 offsetUp = new Vector3(0, 0.1f, 0);
+    Vector3 offsetDown = new Vector3(0, -0.1f, 0);
+
+    float stepDuration = 4f;
 
     public void AE_RunSmoke()
     {
-        Instantiate(runSmoke, transform.position, transform.rotation);
+        GameObject step = Instantiate(runSmoke, transform.position, transform.rotation);
+        Destroy(step, stepDuration);
+    }
+
+    public void OffsetRight_FootStep_Down()
+    {
+        GameObject step = Instantiate(footStep, transform.position + offsetRight, transform.rotation);
+        Destroy(step, stepDuration);
+    }
+
+    public void OffsetLeft_FootStep_Down()
+    {
+        GameObject step = Instantiate(footStep, transform.position + offsetLeft, transform.rotation);
+        Destroy(step, stepDuration);
+    }
+
+    public void OffsetUp_FootStep_Left()
+    {
+        GameObject step = Instantiate(footStep, transform.position + offsetUp, rotation);
+        Destroy(step, stepDuration);
+    }
+
+    public void OffsetDown_FootStep_Left()
+    {
+        GameObject step = Instantiate(footStep, transform.position + offsetDown, rotation);
+        Destroy(step, stepDuration);
     }
 }
